2a1d5689de9d73fc035df50199a425a91b01ba63
Harini Allu <120156889+alluharini@users.noreply.github.com>
2023-11-21 09:03:06 +0530

    Update and rename 8queens.py to 2.8queens.py

--- 2.8queens.py
+++ 2.8queens.py
@@ -1,39 +1,22 @@
-# Python code to display the way from the root  
-# node to the final destination node for N*N-1 puzzle  
-# algorithm by the help of Branch and Bound technique  
-# The answer assumes that the instance of the  
-# puzzle can be solved  
+ import copy  
   
-# Importing the 'copy' for deepcopy method  
-import copy  
-  
-# Importing the heap methods from the python  
-# library for the Priority Queue  
 from heapq import heappush, heappop  
   
-# This particular var can be changed to transform  
-# the program from 8 puzzle(n=3) into 15  
-# puzzle(n=4) and so on ...  
 n = 3  
   
 # bottom, left, top, right  
 rows = [ 1, 0, -1, 0 ]  
 cols = [ 0, -1, 0, 1 ]  
-  
-# creating a class for the Priority Queue  
+   
 class priorityQueue:  
-      
-    # Constructor for initializing a  
-    # Priority Queue  
+       
     def __init__(self):  
         self.heap = []  
   
     # Inserting a new key 'key'  
     def push(self, key):  
         heappush(self.heap, key)  
-  
-    # funct to remove the element that is minimum,  
-    # from the Priority Queue  
+   
     def pop(self):  
         return heappop(self.heap)  
   
@@ -49,34 +32,16 @@ class nodes:
       
     def __init__(self, parent, mats, empty_tile_posi,  
                 costs, levels):  
-                      
-        # This will store the parent node to the  
-        # current node And helps in tracing the  
-        # path when the solution is visible  
+ 
         self.parent = parent  
-  
-        # Useful for Storing the matrix  
-        self.mats = mats  
-  
-        # useful for Storing the position where the  
-        # empty space tile is already existing in the matrix  
+        self.mats = mats   
         self.empty_tile_posi = empty_tile_posi  
-  
-        # Store no. of misplaced tiles  
-        self.costs = costs  
-  
-        # Store no. of moves so far  
+        self.costs = costs   
         self.levels = levels  
-  
-    # This func is used in order to form the  
-    # priority queue based on  
-    # the costs var of objects  
+ 
     def __lt__(self, nxt):  
         return self.costs < nxt.costs  
   
-# method to calc. the no. of  
-# misplaced tiles, that is the no. of non-blank  
-# tiles not in their final posi  
 def calculateCosts(mats, final) -> int:  
       
     count = 0  
@@ -91,7 +56,6 @@ def calculateCosts(mats, final) -> int:
 def newNodes(mats, empty_tile_posi, new_empty_tile_posi,  
             levels, parent, final) -> nodes:  
                   
-    # Copying data from the parent matrixes to the present matrixes  
     new_mats = copy.deepcopy(mats)  
   
     # Moving the tile by 1 position  
@@ -107,8 +71,7 @@ def newNodes(mats, empty_tile_posi, new_empty_tile_posi,
     new_nodes = nodes(parent, new_mats, new_empty_tile_posi,  
                     costs, levels)  
     return new_nodes  
-  
-# func to print the N by N matrix  
+   
 def printMatsrix(mats):  
       
     for i in range(n):  
@@ -117,8 +80,6 @@ def printMatsrix(mats):
               
         print()  
   
-# func to know if (x, y) is a valid or invalid  
-# matrix coordinates  
 def isSafe(x, y):  
       
     return x >= 0 and x < n and y >= 0 and y < n  
@@ -150,22 +111,13 @@ def solve(initial, empty_tile_posi, final):
     # Adding root to the list of live nodes  
     pq.push(root)  
   
-    # Discovering a live node with min. costs,  
-    # and adding its children to the list of live  
-    # nodes and finally deleting it from  
-    # the list.  
     while not pq.empty():  
   
-        # Finding a live node with min. estimatsed  
-        # costs and deleting it form the list of the  
-        # live nodes  
         minimum = pq.pop()  
   
         # If the min. is ans node  
         if minimum.costs == 0:  
-              
-            # Printing the path from the root to  
-            # destination;  
+               
             printPath(minimum)  
             return  
   
@@ -187,23 +139,15 @@ def solve(initial, empty_tile_posi, final):
                 # Adding the child to the list of live nodes  
                 pq.push(child)  
   
-# Main Code  
-  
-# Initial configuration  
-# Value 0 is taken here as an empty space  
+
 initial = [ [ 1, 2, 3 ],  
             [ 5, 6, 0 ],  
             [ 7, 8, 4 ] ]  
   
-# Final configuration that can be solved  
-# Value 0 is taken as an empty space  
+  
 final = [ [ 1, 2, 3 ],  
         [ 5, 8, 6 ],  
         [ 0, 7, 4 ] ]  
   
-# Blank tile coordinates in the   
-# initial configuration  
-empty_tile_posi = [ 1, 2 ]  
-  
-# Method call for solving the puzzle  
+empty_tile_posi = [ 1, 2 ]   
 solve(initial, empty_tile_posi, final)  
